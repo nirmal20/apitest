041e92bbdcad2d42e766808069c9d230f606eae3
Nirmal Hasmukh Dholakhia <>
2020-07-16 14:29:34 +0200

    Refactoring of the code,adding few more assertions  and making custom functions for reusable code

--- cypress/integration/refactor.js
+++ cypress/integration/refactor.js
@@ -2,12 +2,12 @@
 
 context('tests for checking comments ', () => {
 
-    //let idValue
+    
 
-    function validateEmail(email) {
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }
+    // function validateEmail(email) {
+    //     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //     return re.test(String(email).toLowerCase());
+    // }
 
 
     it('get user details', () => {
@@ -39,23 +39,44 @@ context('tests for checking comments ', () => {
             expect(response.status).to.eq(200);
             expect(response).to.have.property('headers')
             expect(response).to.have.property('duration')
-
-            var length = Object.keys(response.body).length;
-            var array = [];
-            
-            for ( var i = 0; i<length; i++){
-                array.push(response.body[i].id);
-            }
-            console.log(array)
-            cy.wrap(array).as('postCount');
-            console.log(this.postCount);
-
+            cy.writeFile('cypress/fixtures/posts.json',response.body);
+       
         });
     });    
 
     it('get comments from each',function () {
-       // cy.get('@postCount').then((response) => {
-        console.log(this.postCount);
+      
+
+        cy.fixture('posts').then((posts) => {
+           
+           cy.log(posts);
+                for (var i of posts){    
+                    console.log(i.id);
+                    cy.request('/comments?postId='+i.id)
+                    .then((commentResponse) => {
+                        //console.log(commentResponse.body)
+                        var commentsCount = Object.keys(commentResponse.body).length;
+                        console.log(commentsCount);
+                        for(var x=0;x<commentsCount;x++){
+                          //  console.log(commentResponse.body[x].email);
+                          cy.validateEmail(commentResponse.body[x].email)
+                          .then((emailValidationResponse) => {
+                                expect(emailValidationResponse).to.be.true;
+                            });
+                           // expect(validateEmail(commentResponse.body[x].email)).to.be.true;
+                           // var returnEmail = validateEmail(commentResponse.body[x].email);
+                            // if(returnEmail == true)
+                            //     console.log("Email Address "+ commentResponse.body[x].email + "is of Valid format");
+                            //  else{
+                            //      console.log("Email Address "+ commentResponse.body[x].email + "is of NOT Valid format");
+                            //  }   
+                    }           
+                });
+             }
+
+        }) 
+
+
     //    var length = Object.keys(this.postCount).length;
     //         for (var i=0;i<length;i++){
     //             cy.request('/comments?postID='+this.postCount[i])
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,6 +11,11 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
+
+    Cypress.Commands.add('validateEmail',(email) => {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    })
 //
 //
 // -- This is a child command --
